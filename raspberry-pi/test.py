92628d86ae6b11be02411fc4fe4b74cca26a0c59
rareval48 <rareval48@charlottesvilleschools.org>
2023-03-08 09:16:03 -0500

    x

--- raspberry-pi/test.py
+++ raspberry-pi/test.py
@@ -1,20 +1,44 @@
 #type: ignore
 import adafruit_mpu6050 as imu
-import busio
 import board
 import time
+import busio
+import pwmio
 
-sdaPin = board.GP2  # defining the SDA & SCL pins to use
-sclPin = board.GP3
-i2c = busio.I2C(sclPin, sdaPin)
-mpu = imu.MPU6050(i2c) 
+sda_pin = board.GP4
+scl_pin = board.GP5
+i2c = busio.I2C(scl_pin, sda_pin)
+mpu = imu.MPU6050(i2c, address = 0x68)
+
+FREQ = 1000
+
+motor1_f = pwmio.PWMOut(board.GP18, frequency=FREQ)
+motor2_f = pwmio.PWMOut(board.GP19, frequency=FREQ)
 
 while True:
 
-    accelerationVals = mpu.acceleration
-    
-    print(f"X acceleration: {accelerationVals[0]} m/s2")
-    print(f"Y acceleration: {accelerationVals[1]} m/s2")
-    print(f"Z acceleration: {accelerationVals[2]} m/s2")
+    First = f"Angular Velocities`:"
+    Second = f"x velocty: {round(mpu.acceleration[0], 1)}"  # round decimals to the 3rd decimal place
+    Third = f"y velocity: {round(mpu.acceleration[1], 1)}"
+    Fourth = f"z velocity: {round(mpu.acceleration[2], 1)}"
+
+    print(f"X acceleration: {mpu.acceleration[0]} m/s2") #print XYZ vals
+    print(f"Y acceleration: {mpu.acceleration[1]} m/s2")
+    print(f"Z acceleration: {mpu.acceleration[2]} m/s2")
     print("")
     time.sleep(.1)
+    
+    if mpu.acceleration[0] <= .3: 
+        print("Level")
+    
+    if mpu.acceleration[0] >= 1: 
+        print("Tilting Right")
+
+    if mpu.acceleration[0] >= -1 <= 0: 
+        print("Tilting Left")
+    
+    if mpu.acceleration[1] <= -1: 
+        print("Tilting Back")
+
+    if mpu.acceleration[1] >= 1: 
+        print("Tilting Forward")
